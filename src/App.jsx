18dbcef6886581f474b1850a35551578e668ac7e
VijayMakkad <vijaymakkad0104@gmail.com>
2024-04-21 12:42:11 +0530

    Creation Operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState,useEffect } from 'react'
 import React from 'react'
 import './App.css'
+import './index.css'
 import { supabase } from './createClient'
 
 function App() {
 
   const [users,setUsers]=useState([])
+  const [user,setUser]=useState({name:'',age:''})
   // console.log(users)
 
   useEffect(()=>{
     fetchUsers()  
   },[])
 
+  function handleChange(e){
+    setUser({...user,[e.target.name]:e.target.value})
+  }
 
+//Read Operation
   async function fetchUsers(){
     const {data,error}=await supabase.from("users").select('*')
     setUsers(data)
@@ -22,10 +28,35 @@ function App() {
     }
   }
 
+  //Creation Operation
+
+  async function createUsers(){
+    const {error}=await supabase.from('users').insert({name:user.name,age:parseInt(user.age)})
+    if(error)console.log(error)
+    else{
+      fetchUsers()
+      console.log('User Created Successfully')
+    }
+  }
+
   return (
     <>
     <div className='heading'>
       <h1>Learning CRUD Operations</h1>
+<div className='creation'>
+
+    <h2>Creation</h2>
+
+    <form onSubmit={createUsers}>
+      <h4>Name</h4>
+      <input type="text" placeholder='Name' name='name' onChange={handleChange} />
+      <h4>Age</h4>
+      <input type="number" placeholder='Age' name='age' onChange={handleChange} />
+      <button type='submit'>Create</button>
+    </form>
+</div>
+
+<h1>Table</h1>
       <table>
         <thead>
           <tr>
@@ -37,7 +68,7 @@ function App() {
 
         <tbody>
           {users.map((users)=>
-            <tr>
+            <tr key={users.id}>
               <td>{users.id}.</td>
               <td>{users.name}</td>
               <td>{users.age}</td>
